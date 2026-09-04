5e601a5b587f51f5c089d6585a6bd6476509d428
Vikasj1809 <vikasjaiswal1809@gmail.com>
2026-09-05 01:46:56 +0530

    Cleanup activity

--- src/com/airtribe/library/service/BookSearchService.java
+++
@@ -1,9 +0,0 @@
-package com.airtribe.library.service;
-
-import com.airtribe.library.entity.Book;
-
-import java.util.List;
-
-public interface BookSearchService {
-    List<Book> search(List<Book> bookList, String searchKey);
-}
--- src/com/airtribe/library/service/RecommendationStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.airtribe.library.service;
-
-import com.airtribe.library.entity.Patron;
-
-import java.util.List;
-
-public interface RecommendationStrategy {
-    List<String> recommend(Patron patron, List<Patron> allPatrons);
-}
